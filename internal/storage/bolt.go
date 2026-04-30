330b6026c04a504ee728b8600e3a0a4b6005385a
Anirudh <shivammm.hyphen@gmail.com>
2026-04-30 06:14:28 +0530

    add: Bolt-Store

---
+++ internal/storage/bolt.go
@@ -0,0 +1,64 @@
+package storage
+
+import (
+	"encoding/json"
+	"errors"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+const bucketName = "secrets"
+
+type BoltStore struct {
+	db *bolt.DB
+}
+
+func NewBoltStore(path string) (*BoltStore, error) {
+	db, err := bolt.Open(path, 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+		return err
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &BoltStore{
+		db: db,
+	}, nil
+}
+
+func (b *BoltStore) Put(key string, value string) error {
+	return b.db.Update(func(tx *bolt.Tx) error {
+
+		bucket := tx.Bucket([]byte(bucketName))
+		data, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+
+		return bucket.Put([]byte(key), data)
+	})
+}
+
+func (b *BoltStore) Get(key string) (string, error) {
+	var value string
+
+	err := b.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+
+		data := bucket.Get([]byte(key))
+		if data == nil {
+			return errors.New("not found")
+		}
+
+		return json.Unmarshal(data, &value)
+	})
+
+	return value, err
+}
