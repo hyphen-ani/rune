f847abe9ef977747b130d8ef5cb4065f9980093a
Anirudh <150090561+hyphen-ani@users.noreply.github.com>
2026-05-08 21:18:26 +0530

    changed script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-VERSION="v0.2.0"
+VERSION="v0.2.0-dev"
 REPO="https://github.com/hyphen-ani/rune/releases/download"
 
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
-if [ "$ARCH" = "amd64" ]; then
-  ARCH="amd64"
-elif [ "$ARCH" = "x86_64" ]; then
-  ARCH="amd64"
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"
+elif [ "$ARCH" = "arm64" ] || [ "$ARCH" = "aarch64" ]; then
+    ARCH="arm64"
 fi
 
 RUNE_BINARY="rune-${OS}-${ARCH}"
@@ -19,15 +19,18 @@ SERVER_BINARY="rune-server-${OS}-${ARCH}"
 
 TMP_DIR=$(mktemp -d)
 
-echo "Downloading Rune Lock...."
+echo "Detected OS: $OS"
+echo "Detected ARCH: $ARCH"
 
-curl -L "$REPO/$VERSION/$RUNE_BINARY" -o "$TMP_DIR/rune"
-curl -L "$REPO/$VERSION/$SERVER_BINARY" -o "$TMP_DIR/rune-server"
+echo "Downloading Rune..."
+
+curl -fL "$REPO/$VERSION/$RUNE_BINARY" -o "$TMP_DIR/rune"
+curl -fL "$REPO/$VERSION/$SERVER_BINARY" -o "$TMP_DIR/rune-server"
 
 chmod +x "$TMP_DIR/rune"
 chmod +x "$TMP_DIR/rune-server"
 
-echo "Installing Rune Lock..."
+echo "Installing Rune..."
 
 sudo mv "$TMP_DIR/rune" /usr/local/bin/rune
 sudo mv "$TMP_DIR/rune-server" /usr/local/bin/rune-server
@@ -35,7 +38,7 @@ sudo mv "$TMP_DIR/rune-server" /usr/local/bin/rune-server
 rm -rf "$TMP_DIR"
 
 echo
-echo "Rune installed successfully.. Keep it a secret"
+echo "Rune installed successfully"
 echo
 echo "Run:"
 echo "  rune --version"
